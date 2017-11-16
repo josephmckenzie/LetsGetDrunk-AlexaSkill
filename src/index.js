66ada20b047bf6b8a6b3eef57616e166f6796605
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-11-16 05:55:11 -0500

    changes to start to accomdate the new api im introuducing to the skill

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
-//require('dotenv').config();
-'use strict'
-/* jslint node: true */
-/* jshint esnext: true */
-/* eslint-env es6 */
-// import chai from "chai"
-var mocha = require('mocha');
-var Alexa = require('/usr/local/lib/node_modules/alexa-sdk');
-//  var  mockResponse = require('gamePlay.json');
-var gamePlay = require('./gamePlay.json');
-var APP_ID =  'amzn1.ask.skill.0bd86f38-a7ef-42b1-9a4f-93e1acc4f04c';
+'use strict';
+var Alexa = require('alexa-sdk');
+var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+
+
+
+
+  var https = require('https');
+  var results = '';
+  var personsarray = ['Joe'] || personsarray;
+  var options = {
+        host: 'bsi7688wf2.execute-api.us-east-1.amazonaws.com',
+        path: '/dev/todos',
+		    port: 443
+	};
+
+var callback = function(response) {
+
+  response.on('data', function (chunk) {
+    results += chunk;
+  });
+  response.on('end', function () {
+//    console.log(str,"str");
+var str = JSON.parse(results);
+var phrases = str.Items;
+
+phrases.forEach(function(item) {
+    personsarray.push(item.personsname);
+});
+	console.log(personsarray,"Persons Drinking")
+	});
+};
+var req = https.request(options, callback).end();
+
+
+
+
+
+
+
+
+
 var languageStrings = {
     "en": {
         "translation": {
-             "SHOTS": [
+             "FACTS": [
+              "Pineapple Upsidedown cake",
 							 "Tequlia",
 							 "southern comfort",
 							 "Whipped Vodka",
@@ -25,37 +57,19 @@ var languageStrings = {
 							 "Moonshine",
 							 "Sex on the beach"
             ],
-           "BEERS":[
-                "Budweiser",
-                "Bud-Light",
-                "Miller",
-                "Yuengling",
-                "Redds Apple Ale",
-                "Jons British Ale",
-                "Four LOKO" 
-            ],
-         
-					"people": [
-						"Joe",
-           "Eddie"
-//						"Marv",
-//						"Jen",
-//						"Heather",
-//						"Josh",
-//						"Aaron",
-//						"Jon",
-//						"Amanda"
-					],
-            "SKILL_NAME" : "Shots in English",
-            "upNext" : "Who's up next? ....",
-            "HELP_MESSAGE" : "Do you really need help getting drunk?, Just say Alexa open wanna get drunk",
-            "HELP_REPROMPT" : "Really man? Wow is all Ican say. You can say Alexa open wanna get drunk",
-            "STOP_MESSAGE" : "Quiting already? Are you sure you want to quit?"
+					  "names": personsarray,
+					
+            "SKILL_NAME" : "Whose Shot, is it anyway?",
+            "GET_FACT_MESSAGE" : "",
+            "HELP_MESSAGE" : "Help taking a shot who are we Marv?",
+            "HELP_REPROMPT" : "Hey everyone !!! Marv needs help",
+            "STOP_MESSAGE" : "Stop taking shots? What is it your bedtime? "
         }
     },
     "en-US": {
         "translation": {
-           "SHOTS": [
+           "FACTS": [
+              "Pineapple Upsidedown cake",
 							 "Tequila",
 							 "southern comfort",
 							 "Whipped Vodka",
@@ -68,39 +82,21 @@ var languageStrings = {
 						 	  "Moonshine",
 							 "Sex on the beach"
             ],
-         "BEERS":[
-                "Budweiser",
-                "Bud-Light",
-                "Miller",
-                "Yuengling",
-                "Redds Apple Ale",
-                "Jons British Ale",
-                "Pabst blue ribbon " 
-            ],
-						"names": [
-						"Joe",
-             "Eddie"
-//						"Marv",
-//						"Jen",
-//						"Heather",
-//						"Josh",
-//						"Aaron",
-//						"Jon",
-//						"Amanda"
-							],
+						"names": personsarray,
 					
-            "SKILL_NAME" : "Shots in en-us",
-            "upNext" : "Who's up next? ....",
-            "HELP_MESSAGE" : "Do you really need help getting drunk?, Just say Alexa open wanna get drunk",
-            "HELP_REPROMPT" : "Really man? Wow is all I can say. You can say Alexa open wanna get drunk",
-            "STOP_MESSAGE" : "Quiting already? Are you sure you want to quit?"
+            "SKILL_NAME" : "Whose Shot, is it anyway?",
+            "GET_FACT_MESSAGE" : "",
+            "HELP_MESSAGE" : "Help taking a shot who are we Marv?",
+            "HELP_REPROMPT" : "Hey everyone !!! Marv needs help",
+            "STOP_MESSAGE" : "Stop taking shots? What is it your bedtime? "
         }
     },
     "en-GB": {
         "translation": {
          						
-						"SHOTS": [
-						//This is where we would switch up the sayings a little bit based on how brits/americans and other cultures say something a bit different .
+						"FACTS": [
+						//This is where we would would switch up the sayings a little bit based on how brits/americans and other cultures say something a bit different .
+               "Pineapple Upsidedown cake",
 							 "Tequila",
 							 "southern comfort",
 							 "Whipped Vodka",
@@ -110,225 +106,61 @@ var languageStrings = {
 							 "You pick it asshole",
 							 "Vegas Bomb",
 							 "JagerBomb",
-						 	 "Moonshine",
+						 	  "Moonshine",
 							 "Sex on the beach"
             ],
-         "BEERS":[
-                "Budweiser",
-                "Bud-Light",
-                "Miller",
-                "Yuengling",
-                "Redds Apple Ale",
-                "Jons British Ale",
-                "Some Kind of IPA" 
-            ],
-					"names": [
-						"Joe",
-						"Marv",
-						"Jen",
-						"Heather",
-						"Josh",
-						"Aaron",
-						"Jon",
-						"Amanda"
-							],
-            "SKILL_NAME" : "Jons British Shot Game",
-            "upNext" : "Who's up next? ....",
-            "HELP_MESSAGE" : "I know your british but come on man it's not that hard. Just say Alexa open wanna get drunk",
-            "HELP_REPROMPT" : "Wow .... Just wow that is so lame that you must be Jon",
-            "STOP_MESSAGE" : "Jon is lame... but still are you quitting already?"
+					  "names": personsarray,
+					
+            "SKILL_NAME" : "Whose Shot, is it anyway?",
+            "GET_FACT_MESSAGE" : "",
+            "HELP_MESSAGE" : "Jon Says to Google it",
+            "HELP_REPROMPT" : "Whilst you're waiting for me to help, Google it",
+            "STOP_MESSAGE" : "Jon is the DD tonight and has to abstain from farther shots."
         }
     },
  };
-var attrArray = [];
-var attributes ;
-var name = "name";
-var sessionSpot;
-var speechResponse;
-var speechReprompt;
-var handlers = {
- 
-    'LaunchRequest': function () {
-//       console.log("Initial App launched,Alexa Asks: Can you handle Alexa?");
-      sessionSpot = 'Initial App launched, Alexa Asks: Can you handle Alexa?';
-      attributes = {} ;
-      attributes[name] = sessionSpot;
-      attrArray.push(attributes);
-      speechResponse = 'Sure, but do you really think that you can handle Alexa?';
-      speechReprompt = "How on earth do you need a reprompt message for the intital launch of me?";
-      console.log(attributes,"Attributes Hash for this session");
-      console.log(attrArray,"Attributes Array of hashes");
-      
-     this.emit(':ask', speechResponse,speechReprompt,attributes);
-    },
-    
-    'Unhandled': function() {
-       //console.log("Unhandled Intent");
-      sessionSpot = 'Unhandled Intent';
-      attributes = {} ;
-      attributes[name] = sessionSpot;
-      attrArray.push(attributes);
-      console.log(attributes,"Attributes Hash for this session");
-      console.log(attrArray,"Attributes Array of hashes");
-      
-      this.emit(':ask', 'What did you say? Are you drunk?', "I'll say it slower please repeat your self!");
-    },
- 
-    'AMAZON.YesIntent': function () {
-     //console.log("Alexa Asked: Can you handle Alexa?, Response: Yes");
-     attributes = {} ;
-     sessionSpot = "Alexa Asked: Can you handle Alexa?, Response: Yes";
-//     var yesCount = 1 || yesCount;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     speechResponse ='Awesome, are we drinking Shots or Beer?';
-     speechReprompt = "Ok I'll ask you once more would you prefer to have spme Shots or Beers?";
-      console.log(attributes,"Attributes Hash for this session");
-      console.log(attrArray,"Attributes Array of hashes");
-     this.emit(':ask',speechResponse ,speechReprompt, attributes);
-    },
- 
-    'AMAZON.NoIntent': function () {
-      //console.log("Alexa Asked: Can you handle Alexa?, Response: No");
-     sessionSpot = "Alexa Asked: Can you handle Alexa?, Response: No";
-     attributes = {} ;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     speechResponse ='Well, ok I guess If you know a secret phrase or key word tell me now, or try and tell me to stop. Go ahead try it I dare you to. If want to have some drinks just tell me drinks';
-     speechReprompt = "Are you really sure you don't want to drink? Ok, well either tell me to cancel or quit or maybe you can do something else prehaps?";
-      console.log(attributes,"Attributes Hash for this session");
-      console.log(attrArray,"Attributes Array of hashes");
 
-     this.emit(':ask',speechResponse ,speechReprompt, attributes);
-    },
-
-    'AMAZON.HelpIntent': function() {
-      //console.log("You must be drunk, You asked for help from Alexa?!?");
-     sessionSpot = "You must be drunk, You asked for help from Alexa?!?";
-     attributes = {} ;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     var speechOutput = this.t("HELP_MESSAGE");
-     var reprompt = this.t("HELP_REPROMPT");
-      console.log(attributes,"Attributes Hash for this session");
-      console.log(attrArray,"Attributes Array of hashes");
-     this.emit(':ask', speechOutput, reprompt,attributes);
-      },
-
-    'AMAZON.CancelIntent': function() {
-          //console.log("Cancel Intent");
-     sessionSpot = "Cancel Intent. Why oh why?";
-     attributes = {} ;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     console.log(attributes,"Attributes Hash for this session");
-     console.log(attrArray,"Attributes Array of hashes");
-     this.emit(':tell', "Well Goodbye then and have a wonderful day. You asshole");
-     },
-
-    'AMAZON.StopIntent': function() {
-          //console.log("Stop Intent");
-     sessionSpot = "Stop Intent. You're gonna stop now?";
-     attributes = {} ;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     console.log(attributes,"Attributes Hash for this session");
-     console.log(attrArray,"Attributes Array of hashes");
-      
-         this.emit(':tell', this.t("STOP_MESSAGE"));
-     },
- 
-    'BeerOrShotsIntent': function() {
-              //console.log("Shots or Beer Intent");
-     sessionSpot = "Alexa Asked Can You hang?: User responded Hell yes!, Alexa is now waiting for a response : Beers or Shots?";
-     attributes = {} ;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     console.log(attributes,"Attributes Hash for this session");
-     console.log(attrArray,"Attributes Array of hashes");
-     this.emit(':ask', 'Are we drinking Shots or Beer?', 'Are we drinking Shots or Beer?');
-     },
- 
-    'GetShotsIntent': function() {
-     sessionSpot = "Alexa Asked Shots or beer?: The user decides on: Shots";
-     attributes = {} ;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     console.log(attributes,"Attributes Hash for this session");
-     console.log(attrArray,"Attributes Array of hashes"); 
-     //call the GetDrinks intent passing it an agurment for the type of drink the user chose.
-     //notice we are not saying ask or tell but rather calling the intent we want
-     this.emit('GetDrinks',"SHOTS");
-    },
- 
-    'GetBeersIntent': function() {
-     sessionSpot = "Alexa Asked Shots or beer?: The user responded with : Beers";
-     attributes = {} ;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     console.log(attributes,"Attributes Hash for this session");
-     console.log(attrArray,"Attributes Array of hashes"); 
-     //call the GetDrinks intent passing it an agurment for the type of drink the user chose.
-    //notice we are not saying ask or tell but rather calling the intent we want.
-     this.emit('GetDrinks',"BEERS");
-    },  
- 
-    'SweetMuffinsIntent': function() {
-       // If youave come across this intent then you are pretty cool.
-      //console.log("SweetMuffins Intent");
-     sessionSpot = "You have came across Sweetmuffins, Alexa asks: Which Sweetmufffins?";
-     attributes = {} ;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     console.log(attributes,"Attributes Hash for this session");
-     console.log(attrArray,"Attributes Array of hashes");  
-       this.emit(':ask', 'Are we talking about My sweet muffins from Pennsylvania or from England?');
-    },
- 
-    'DebbieIntent': function() {
-      // Debbie has been chosen that is pretty cool ;D
-     //console.log("Debbies Intent or my Intent with her?");
-     sessionSpot = "Alexa aske: Which Sweetmufffins?, User Responded: Debbie/Pennslyvania";
-     attributes = {} ;
-     attributes[name] = sessionSpot;
-     attrArray.push(attributes);
-     console.log(attributes,"Attributes Hash for this session");
-     console.log(attrArray,"Attributes Array of hashes");  
-        this.emit(':ask', 'Woohoo DEBBIE? Im celebrating tonight!! How are you doing tonight debbie?');
-    },
-    
-    'GoodIntent': function() {
-          // Debbie has been chosen that is pretty cool ;D
-         //console.log("Debbies Intent or my Intent with her?");
-        this.emit(':ask', 'Good to hear, So would you do me a favor and say man buns? If you dont want to just say Pass.');
-    },
- 
-    'JonsMomIntent': function() {
-         //console.log("Jons Mom");
-        this.emit(':tell', 'Jons mom may be far in distance but not in heart ');
-    },
- 
-    'GetDrinks': function(type) {
-        var shotsTaken = 1 || shotsTaken; 
-        shotsTaken += 1;
-			  var people = this.t("names");
-			  var peopleindex = Math.floor(Math.random() * people.length);
-        var shotsTookBy = peopleindex + "has taken" + shotsTaken + "shots";
-        var somoneElse = "Would someone else like one?";
-        var drinkUp = this.t(type);
-			  var drinkOrder = Math.floor(Math.random() * drinkUp.length);
-        var randomDrinkType = people[peopleindex] + ' is with some ' + drinkUp[drinkOrder];
-        var speechOutput = this.t("upNext") + randomDrinkType + shotsTookBy + somoneElse;
-     console.log(shotsTaken);
-        this.emit(':ask', speechOutput, speechOutput, speechOutput);
-    }
-};
-
-exports.handler = function(event, context,callback) {
-    var alexa = Alexa.handler(event, context,callback);
-    alexa.appId = APP_ID;
-    alexa.attributes = attributes;
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
     alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetFact');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+        // Get a random space fact from the space facts list
+        // Use this.t() to get corresponding language data
+       
+			  var people = this.t("names");
+			  var peopleindex = Math.floor(Math.random() * people.length);
+			
+        var factArr = this.t('FACTS');
+			  var factIndex = Math.floor(Math.random() * factArr.length);
+			
+        var randomFact = people[peopleindex] + '....' + factArr[factIndex];
+
+        // Create speech output
+        var speechOutput = this.t("GET_FACT_MESSAGE") + randomFact;
+        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), randomFact)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = this.t("HELP_MESSAGE");
+        var reprompt = this.t("HELP_MESSAGE");
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    }
 };
